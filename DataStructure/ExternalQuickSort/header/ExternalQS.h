3c35abfb8325267b7a15a9adbdda794075329cff
Andrew-Rey <2121204816@qq.com>
2022-10-08 21:36:25 +0800

    external quick sort test 2

--- DataStructure/ExternalQuickSort/header/ExternalQS.h
+++ DataStructure/ExternalQuickSort/header/ExternalQS.h
@@ -33,19 +33,19 @@ public:
                        int bottom, int top, int count);
 
     // read in data from txt, `p_file` is the tail of file
-    void read_to_mid(int file_base, int buf_size, int &p_file);
+    void read_to_mid(int file_base, int buf_size, long long &p_file);
 
     // load data into input buffer
-    void read_to_input(int file_base, int buf_size, int &p_file);
+    void read_to_input(int file_base, int buf_size, long long &p_file);
 
     // write into disk when small or large memo is full
-    void write(const std::string &loc, std::deque<T> buffer, int file_base, int size, int &p_file);
+    void write(const std::string &loc, std::deque<T> &buffer, int file_base, long long &p_file);
 
     // write temp_file to result_file
-    void write_t2r(const std::string &loc_result, const std::string &loc_temp, int file_base, int &p_file);
+    void write_t2r(const std::string &loc_result, const std::string &loc_temp, int file_base, long long &p_file);
 
     // insert element into middle memo
-    void replace(const std::string &loc_result, const std::string &loc_temp, int &p_small, int &p_large);
+    void replace(const std::string &loc_result, const std::string &loc_temp, long long &p_small, long long &p_large);
 
     // sort recursively and store the elements into txt
     void sort();
@@ -56,7 +56,7 @@ public:
 
 template<typename T>
 void
-ExternalQS<T>::read_to_mid(int file_base, int buf_size, int &p_file) {
+ExternalQS<T>::read_to_mid(int file_base, int buf_size, long long &p_file) {
     std::cout << "--function read_to_mid()" << std::endl;
     std::ifstream read_file{loc};
     int read_count = 0;
@@ -67,13 +67,13 @@ ExternalQS<T>::read_to_mid(int file_base, int buf_size, int &p_file) {
         read_file >> temp;
         qs_queue->push(temp);
     }
-    p_file = file_base + buf_size;            // get the file pointer (the last index)
-    std::cout << "size when reading: " << qs_queue->Deque.size() << std::endl;
+//    p_file = file_base + buf_size;            // get the file pointer (the last index)
+    p_file = read_file.tellg();
     read_file.close();
 }
 
 template<typename T>
-void ExternalQS<T>::read_to_input(int file_base, int buf_size, int &p_file) {
+void ExternalQS<T>::read_to_input(int file_base, int buf_size, long long &p_file) {
     std::cout << "--function read_to_input()" << std::endl;
     std::ifstream read_file{loc};
     int read_count = 0;
@@ -82,14 +82,15 @@ void ExternalQS<T>::read_to_input(int file_base, int buf_size, int &p_file) {
     for (read_count; read_count < buf_size && !read_file.eof(); ++read_count) {
         T temp;
         read_file >> temp;
-        input_buf.push_front(temp);
+        input_buf.push_back(temp);
     }
-    p_file = file_base + buf_size; // get the file pointer (the last index)
+//    p_file = file_base + buf_size; // get the file pointer (the last index)
+    p_file = read_file.tellg();
     read_file.close();
 }
 
 template<typename T>
-void ExternalQS<T>::write(const std::string &_loc, std::deque<T> buffer, int file_base, int size, int &p_file) {
+void ExternalQS<T>::write(const std::string &_loc, std::deque<T> &buffer, int file_base, long long &p_file) {
     std::cout << "----function write()" << std::endl;
     if (buffer.empty())
         return;
@@ -99,31 +100,35 @@ void ExternalQS<T>::write(const std::string &_loc, std::deque<T> buffer, int fil
     for (auto it = buffer.begin(); it != buffer.end(); ++it) {
         write_file << *it << " ";
     }
-    p_file = file_base + size;
+//    p_file = file_base + size;
+    p_file = write_file.tellp();
     write_file.close();
+    buffer.clear();
 }
 
 template<typename T>
-void ExternalQS<T>::write_t2r(const std::string &loc_result, const std::string &loc_temp, int file_base, int &p_file) {
+void
+ExternalQS<T>::write_t2r(const std::string &loc_result, const std::string &loc_temp, int file_base, long long &p_file) {
     std::cout << "----function write_t2r()" << std::endl;
     std::ofstream result_file{loc_result};
     std::ifstream temp_file{loc_temp};
-    int size = 0;
-    assert(result_file.is_open() || temp_file.is_open());
+//    int size = 0;
+    assert(result_file.is_open() && temp_file.is_open());
     result_file.seekp(file_base, std::ios::beg);
     while (!temp_file.eof()) {
         T elem;
         temp_file >> elem;
         result_file << elem << " ";
-        size += 1;
     }
-    p_file = file_base + size;
+//    p_file = file_base + size;
+    p_file = result_file.tellp();
     result_file.close();
     temp_file.close();
 }
 
 template<typename T>
-void ExternalQS<T>::replace(const std::string &loc_result, const std::string &loc_temp, int &p_small, int &p_large) {
+void ExternalQS<T>::replace(const std::string &loc_result, const std::string &loc_temp, long long &p_small,
+                            long long &p_large) {
     std::cout << "------function replace()" << std::endl;
     // the input two pointers means where to start
     // the return of the two pointers means the size
@@ -143,33 +148,31 @@ void ExternalQS<T>::replace(const std::string &loc_result, const std::string &lo
         T input_elem = input_buf.front();
         input_buf.pop_front();
 
+        if (small_buf.size() == SMALL_SIZE) {
+            // small_buf write to result file
+            write(result_loc, small_buf, p_small, p_small);
+        }
+        if (large_buf.size() == LARGE_SIZE) {
+            // large_buf write to temp file, write to result file after middle data is written
+            write(temp_loc, large_buf, p_large, p_large);
+        }
         if (input_elem < mid_min) {
-            small_buf.push_front(input_elem);
-            int small_size = small_buf.size();
             // write back the small part when small_buf is full
-            if (small_buf.size() == SMALL_SIZE) {
-                // small_buf write to result file
-                write(result_loc, small_buf, p_small, SMALL_SIZE, p_small);
-            }
+            small_buf.push_back(input_elem);
         } else if (input_elem > mid_max) {
-            large_buf.push_front(input_elem);
-            int large_size = large_buf.size();
             // write back the large part when large_buf is full or all data has been detected
-            if (large_buf.size() == LARGE_SIZE) {
-                // large_buf write to temp file, write to result file after middle data is written
-                write(temp_loc, large_buf, p_large, LARGE_SIZE, p_large);
-            }
+            large_buf.push_back(input_elem);
         } else {
             std::cout << "deque size = " << qs_queue->getElemCount() << std::endl;
             if (qs_queue->getElemCount() == MIDDLE_SIZE) {
                 if (flag) {
-                    T pop_min =  qs_queue->getMin();
-                    small_buf.push_front(qs_queue->getMin());
+                    T pop_min = qs_queue->getMin();
+                    small_buf.push_back(qs_queue->getMin());
                     qs_queue->popMin();
                     flag = false;
                 } else {
                     T pop_max = qs_queue->getMax();
-                    large_buf.push_front(qs_queue->getMax());
+                    large_buf.push_back(qs_queue->getMax());
                     qs_queue->popMax();
                     flag = true;
                 }
@@ -184,7 +187,7 @@ void ExternalQS<T>::sort() {
     std::cout << "function sort()" << std::endl;
     std::deque<int> indices;
     indices.push_front(0);  // storage left flag
-    indices.push_back(qs_queue->Deque.size() - 1);  // storage right flag
+    indices.push_back(DATA_SIZE - 1);  // storage right flag
 
     while (!indices.empty()) {
         int left = indices.front();
@@ -192,55 +195,49 @@ void ExternalQS<T>::sort() {
         indices.pop_front();
         indices.pop_back();
 
-        int p_file = 0;  // used for original file
-        int p_small = 0; // used for write small to file
-        int p_large = 0; // used for write large to file
+        long long p_file = 0;  // used for original file
+        long long p_small = 0; // used for write small to file
+        long long p_large = 0; // used for write large to file
 
         int data_count = 0;
         int small_count = 0;
         int large_count = 0;
+        int middle_size = std::min(DATA_SIZE - data_count, MIDDLE_SIZE);
+        read_to_mid(left, middle_size, p_file);
+        data_count += qs_queue->getElemCount();
 
-        read_to_mid(left, MIDDLE_SIZE, p_file);
-        data_count += p_file;
+        while (data_count < right - left + 1) {
 
-        while (data_count < DATA_SIZE) {
-            std::cout << "p_file = " << p_file << std::endl;
-            read_to_input(p_file, INPUT_SIZE, p_file);
-            data_count += p_file;
-            std::cout << "----p_file = " << p_file << std::endl;
+            int input_size = std::min(DATA_SIZE - data_count, INPUT_SIZE);
+            read_to_input(p_file, input_size, p_file);
+            data_count += input_buf.size();
 
             replace(result_loc, temp_loc, p_small, p_large);
-            small_count += p_small;
-            large_count += p_large;
-            std::cout << "p_small = " << p_small << std::endl;
-            std::cout << "p_large = " << p_large << std::endl;
-
-            // if all data have been detected, must be written
-            // file is over but there are still elements in small_buf
-            if (p_file == DATA_SIZE && p_small != SMALL_SIZE) {
-                write(result_loc, small_buf, p_small, small_buf.size(), p_small);
-                small_count += p_small;
-            }
-
-            // file is over but there are still elements in large_buf
-            if (p_file == DATA_SIZE && p_large != LARGE_SIZE) {
-                write(temp_loc, large_buf, p_large, large_buf.size(), p_large);
-                large_count += p_large;
-            }
         }
 
+        write(result_loc, small_buf, p_small, p_small);
+        write(temp_loc, large_buf, p_large, p_large);
+        small_count += small_buf.size();
+        large_count += large_buf.size();
+
         p_file = p_small;
-        qs_queue->write(result_loc, p_file, qs_queue->Deque.size(), p_file);
+        qs_queue->write(result_loc, p_file, p_file);
 
         p_large = p_file;
         write_t2r(result_loc, temp_loc, p_file, p_file);
 
         // after all data has been detected, partition the small and large part again
-        indices.push_front(left);
-        indices.push_back(p_small);
-        indices.push_front(p_large);
-        indices.push_back(right);
-        std::cout << left << " " << p_small << " " << p_large << " " << right << std::endl;
+        // if left part exists
+        if (left < small_count) {
+            indices.push_front(left);
+            indices.push_back(small_count);
+        }
+        // if right part exists
+        if (right > DATA_SIZE - large_count) {
+            indices.push_front(DATA_SIZE - large_count);
+            indices.push_back(right);
+        }
+        std::cout << left << " " << small_count << " " << large_count << " " << right << std::endl;
     }
 }
 
@@ -264,7 +261,7 @@ ExternalQS<T>::generate_rand(const std::string &type_name, int bottom, int top, 
     std::cout << "--function generate_rand()" << std::endl;
     std::ofstream write_file{loc};
     assert(write_file.is_open() && write_file.good());
-    static std::default_random_engine generator(time(nullptr));
+    std::default_random_engine generator(time(nullptr));
     if constexpr (std::is_integral<T>::value) { // confirm the type of data when compiling
         static std::uniform_int_distribution<T> rand_int(bottom, top);
         for (int i = 0; i < count; ++i) {
--- DataStructure/ExternalQuickSort/header/PriorityDeque.h
+++ DataStructure/ExternalQuickSort/header/PriorityDeque.h
@@ -14,6 +14,8 @@ template<typename T>
 struct node {
     T left;
     T right;
+    bool lNull;
+    bool rNull;
 };
 
 template<typename T>
@@ -27,16 +29,13 @@ public:
 
     ~PriorityDeque();
 
-    bool isEmpty();                     // judge the priority deque is empty
-    bool backRightNull();               // if the last node have right element
-    bool backLeftNull();                // if the last node have left element
     void push(T elem);                  // insert elem in back node
     void popMax();                      // delete the max elem and adjust the structure
     void popMin();                      // delete the min elem and adjust the structure
 
     void adjust();
 
-    void write(const std::string &loc, int file_base, int size, int &p_file);
+    void write(const std::string &loc, int file_base, int &p_file);
 
     T getMax();                         // return the max elem
     T getMin();                         // return the min elem
@@ -52,61 +51,39 @@ template<typename T>
 PriorityDeque<T>::~PriorityDeque() = default;
 
 template<typename T>
-bool PriorityDeque<T>::isEmpty() {
-    return Deque.empty();
-}
-
-template<typename T>
-bool PriorityDeque<T>::backLeftNull() {
-    if (elemCount % 2 == 0) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-template<typename T>
-bool PriorityDeque<T>::backRightNull() {
-    if (elemCount % 2 == 1) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-template<typename T>
 void PriorityDeque<T>::adjust() {
     std::cout << "****function adjust()" << std::endl;
     if (elemCount == 0) {
         return;
-    } else if (elemCount % 2 == 0) {
-        // elemCount >= 2, even
-        int child;
-        int parent;
-        for (child = Deque.size() - 1, parent = (Deque.size() - 1 - (Deque.size() % 2)) / 2;
-             parent >= 0;
-             child = parent, parent = (parent - ((parent + 1) % 2)) / 2) {
-            if (Deque.at(child).left > Deque.at(child).right && child != Deque.size() - 1) {
-                std::swap(Deque.at(child).left, Deque.at(child).right);
-            }
-            if (Deque.at(child).left < Deque.at(parent).left) {
-                std::swap(Deque.at(child).left, Deque.at(parent).left);
-            } else {
-                break;
-            }
-        }
     } else {
-        // elemCount % 2 == 1
+        if (Deque.size() == 1 && !Deque.front().lNull && !Deque.front().rNull &&
+            Deque.front().left > Deque.front().right) {
+            std::swap(Deque.front().left, Deque.front().right);
+            return;
+        }
         int child;
         int parent;
-        for (child = Deque.size() - 1, parent = (Deque.size() - 1 - (Deque.size() % 2)) / 2;
-             parent >= 0;
-             child = parent, parent = (parent - ((parent + 1) % 2)) / 2) {
-            if (Deque.at(child).left > Deque.at(child).right && child != Deque.size() - 1) {
+        for (child = Deque.size() - 1, parent = (Deque.size() - 2) / 2;
+             child > parent && parent >= 0;
+             child = parent, parent = (parent - 1) / 2) {
+
+            if (Deque.at(child).left > Deque.at(child).right &&
+                !Deque.at(child).lNull &&
+                !Deque.at(child).rNull) {
+                // both right and left isn't null, left > right
                 std::swap(Deque.at(child).left, Deque.at(child).right);
             }
-            if (Deque.at(child).right > Deque.at(parent).right) {
+
+            if (Deque.at(child).right > Deque.at(parent).right &&
+                !Deque.at(child).rNull) {
+                // compare right value when:
+                // child.right is not null
                 std::swap(Deque.at(child).right, Deque.at(parent).right);
+            } else if (Deque.at(child).left < Deque.at(parent).left &&
+                       !Deque.at(child).lNull) {
+                // compare left value when:
+                // child.left is not null
+                std::swap(Deque.at(child).left, Deque.at(parent).left);
             } else {
                 break;
             }
@@ -117,18 +94,41 @@ void PriorityDeque<T>::adjust() {
 template<typename T>
 void PriorityDeque<T>::push(T elem) {
     std::cout << "******function push()" << std::endl;
-    if (elemCount % 2 == 0) {
+    int init = 0;
+    if (elemCount == 0) {
         node<T> newNode;
-        int init = 0;
-        newNode.right = *(T *) &init;
-        newNode.left = elem;
         Deque.push_back(newNode);
+        Deque.back().left = elem;
+        Deque.back().right = *(T *) &init;
+        Deque.back().rNull = true;
+        Deque.back().lNull = false;
+        elemCount += 1;
+        return;
+    } else if (elemCount % 2 == 0) {
+        node<T> newNode;
+        Deque.push_back(newNode);
+        int parent = (Deque.size() - 2) / 2;
+        if (elem > Deque.at(parent).right) {
+            Deque.back().right = elem;
+            Deque.back().rNull = false;
+            Deque.back().left = *(T *) &init;
+            Deque.back().lNull = true;
+        } else {
+            // default, insert in left
+            Deque.back().left = elem;
+            Deque.back().lNull = false;
+            Deque.back().right = *(T *) &init;
+            Deque.back().rNull = true;
+        }
         elemCount += 1;
     } else {
         // elemCount % 2 == 1
-        Deque.back().right = elem;
-        if (Deque.back().left > Deque.back().right) {
-            std::swap(Deque.back().left, Deque.back().right);
+        if (Deque.back().lNull) {
+            Deque.back().left = elem;
+            Deque.back().lNull = false;
+        } else {
+            Deque.back().right = elem;
+            Deque.back().rNull = false;
         }
         elemCount += 1;
     }
@@ -144,48 +144,102 @@ void PriorityDeque<T>::popMax() {
     if (Deque.size() == 1 && elemCount == 1) {
         Deque.pop_front();
         elemCount -= 1;
+        return;
     } else if (Deque.size() == 1 && elemCount == 2) {
         int init = 0;
         Deque.front().right = *(T *) &init;
+        Deque.front().rNull = true;
         elemCount -= 1;
+        return;
     } else {
         if (elemCount % 2 == 0) {
+            int init = 0;
             Deque.front().right = Deque.back().right;
+            Deque.back().right = *(T *) &init;
+            Deque.back().rNull = true;
         } else {
             Deque.front().right = Deque.at(Deque.size() - 2).right;
             Deque.at(Deque.size() - 2).right = Deque.back().left;
             Deque.pop_back();
         }
         elemCount -= 1;
-        adjust();
+        // rebuild from top to bottom
+        int parent = 0;
+        int lchild = parent * 2 + 1;
+        int rchild = parent * 2 + 2;
+        int next = parent;
+        while (lchild < Deque.size()) {
+            if (Deque.at(lchild).right > Deque.at(parent).right) {
+                next = lchild;
+            }
+            if (rchild < Deque.size() &&
+                Deque.at(rchild).right > Deque.at(parent).right &&
+                Deque.at(lchild).right < Deque.at(rchild).right) {
+                next = rchild;
+            }
+            if (parent != next) {
+                std::swap(Deque.at(parent).right, Deque.at(next).right);
+            } else {
+                break;
+            }
+            parent = next;
+            lchild = parent * 2 + 1;
+            rchild = parent * 2 + 2;
+        }
+        return;
     }
 }
 
 template<typename T>
 void PriorityDeque<T>::popMin() {
     std::cout << "**function popMin()" << std::endl;
-    int init = 0;
     if (Deque.empty()) {
         throw std::runtime_error("empty Deque can't popMax");
     }
     if (Deque.size() == 1 && elemCount == 1) {
         Deque.pop_front();
         elemCount -= 1;
+        return;
     } else if (Deque.size() == 1 && elemCount == 2) {
+        int init = 0;
         Deque.front().left = Deque.front().right;
         Deque.front().right = *(T *) &init;
+        Deque.front().rNull = true;
         elemCount -= 1;
+        return;
     } else {
         if (elemCount % 2 == 0) {
             Deque.front().left = Deque.back().left;
-            Deque.back().left = Deque.back().right;
-            Deque.back().right = *(T *) &init;
+            Deque.back().lNull = true;
         } else {
             Deque.front().left = Deque.back().left;
             Deque.pop_back();
         }
         elemCount -= 1;
-        adjust();
+        // rebuild from top to bottom
+        int parent = 0;
+        int lchild = parent * 2 + 1;
+        int rchild = parent * 2 + 2;
+        int next = parent;
+        while (lchild < Deque.size()) {
+            if (Deque.at(lchild).left < Deque.at(parent).left) {
+                next = lchild;
+            }
+            if (rchild < Deque.size() &&
+            Deque.at(rchild).left < Deque.at(parent).left &&
+            Deque.at(rchild).left < Deque.at(lchild).left) {
+                next = rchild;
+            }
+            if (next != parent) {
+                std::swap(Deque.at(parent).left, Deque.at(next).left);
+            } else {
+                break;
+            }
+            parent = next;
+            lchild = parent * 2 + 1;
+            rchild = parent * 2 + 2;
+        }
+        return;
     }
 }
 
@@ -211,7 +265,7 @@ int PriorityDeque<T>::getElemCount() {
 }
 
 template<typename T>
-void PriorityDeque<T>::write(const std::string &loc, int file_base, int size, int &p_file) {
+void PriorityDeque<T>::write(const std::string &loc, int file_base, int &p_file) {
     std::cout << "function Priority::write()" << std::endl;
     if (Deque.empty()) {
         return;
@@ -224,6 +278,7 @@ void PriorityDeque<T>::write(const std::string &loc, int file_base, int size, in
         popMin();
         write_file << value << " ";
     }
-    p_file = write_file.tellp() / sizeof(T) - 1;
+    p_file = write_file.tellp();
     write_file.close();
+    Deque.clear();
 }
