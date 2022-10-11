775d2620939c823e1f95bcd845cbc05a6ad6046d
Andrew-Rey <2121204816@qq.com>
2022-10-11 10:34:38 +0800

    initial commit

---
+++ intro-to-ai/project1/main.cpp
@@ -0,0 +1,72 @@
+#include <ctime>
+#include <iostream>
+#include <vector>
+#include <functional>
+#include "Problem.h"
+#include "Search.h"
+#include "Action.h"
+
+typedef std::vector<Action> (*searchFunc)(Problem &problem);
+
+void printStats(std::vector<std::pair<int, float>> &statistics) {
+    std::cout << "length, seconds" << '\n';
+    for (auto &pair: statistics) {
+        std::cout << pair.first << ", " << pair.second << '\n';
+    }
+}
+
+void doExperiment(int iteration, int problemSize, int suffleSteps, searchFunc searchFunc) {
+    std::vector<std::pair<int, float>> statistics;
+
+    for (int i = 0; i < iteration; i++) {
+        Problem problem(problemSize, suffleSteps);
+        problem.printState(problem.initState);
+
+        clock_t t = clock();
+        std::vector<Action> solution = searchFunc(problem);
+        t = clock() - t;
+
+        std::pair<int, float> stats(solution.size(), ((float) t) / CLOCKS_PER_SEC);
+        statistics.push_back(stats);
+
+        printf("Searching tooks %f seconds.\n", stats.second);
+        problem.printSolution(solution);
+
+        problem.updateInitState();
+    }
+
+    printStats(statistics);
+}
+
+void testSearchFunction(searchFunc searchFunc) {
+    int iteration = 50;
+    int size = 3;
+    int suffleSteps = 30;
+
+    for (int i = 1; i <= iteration; i++) {
+        Problem problem(size, suffleSteps);
+        std::vector<Action> solution = searchFunc(problem);
+        if (solution == std::vector<Action>({Action::CUTOFF})) {
+            std::cout << "Iteration " << i << ": CUTOFF\n";
+        } else {
+            std::cout << "Iteration " << i << ": ";
+            std::cout << problem.isSolution(solution, problem.initState) << std::endl;
+        }
+        problem.updateInitState();
+    }
+}
+
+
+int main() {
+    std::srand((unsigned int) std::time(NULL)); // Generate a random seed
+
+    doExperiment(50, 3, 10, (searchFunc) search::bfs);
+
+    // 以下函数用于测试搜索函数结果的正确性
+    // testSearchFunction((searchFunc)search::bfs);
+    // testSearchFunction((searchFunc)search::dlsWrapper);
+    // testSearchFunction((searchFunc)search::aStarMisplace);
+    // testSearchFunction((searchFunc)search::aStarManhattan);
+
+    system("pause");
+}
---
+++ intro-to-ai/project1/src/Search.cpp
@@ -0,0 +1,166 @@
+﻿#include "Search.h"
+
+namespace search
+{
+	Node childNode(Problem& problem, Node& parent, Action action, int stepCost)
+	{
+		return Node(&parent, action, parent.cost + stepCost, problem.result(parent.state, action));
+	}
+
+	std::vector<Action> getSolution(Node node)
+	{
+		std::vector<Action> result;
+		while (node.parent != NULL)
+		{
+			result.insert(result.begin(), node.action);
+			node = *node.parent;
+		}
+
+		return result;
+	}
+
+	bool inSet(std::set<std::vector<int>>& collection, std::vector<int>& state)
+	{
+		return collection.find(state) != collection.end();
+	}
+
+	bool inMap(std::map<std::vector<int>, Node*>& map, std::vector<int>& state)
+	{
+		return map.find(state) != map.end();
+	}
+
+	void updateNode(std::priority_queue<Node*, std::vector<Node*>, Node::cmp>& frontier, Node* sameStateNode, Node* child)
+	{
+		sameStateNode->action = child->action;
+		sameStateNode->cost = child->cost;
+		sameStateNode->parent = child->parent;
+		std::vector<Node*> nodes;
+		while (!frontier.empty())
+		{
+			nodes.push_back(frontier.top());
+			frontier.pop();
+		}
+		for (Node* node : nodes)
+		{
+			frontier.push(node);
+		}
+	}
+
+	// Reference: Figure 3.11 Breadth-first search on a graph.
+	std::vector<Action> bfs(Problem& problem)
+	{
+		Node* initNode = new Node(NULL, Action::FAILURE, 0, problem.initState);
+		problem.nodePtrs.insert(initNode);
+
+		if (problem.isGoal(initNode->state)) return std::vector<Action>();
+
+		std::queue<Node*> frontier;
+		frontier.push(initNode);
+		std::set<std::vector<int>> frontierSet;
+		frontierSet.insert(initNode->state);
+		std::set<std::vector<int>> explored;
+
+		while (!frontier.empty())
+		{
+			Node* current = frontier.front();
+
+			frontier.pop();
+			frontierSet.erase(current->state);
+			explored.insert(current->state);
+
+			for (Action action : problem.getValidActions(current->state))
+			{
+				Node* child = new Node(childNode(problem, *current, action, 1));
+				problem.nodePtrs.insert(child);
+				if (!inSet(frontierSet, child->state) && !inSet(explored, child->state))
+				{
+					if (problem.isGoal(child->state)) return getSolution(*child);
+					frontier.push(child);
+					frontierSet.insert(child->state);
+				}
+			}
+		}
+
+		return { Action::FAILURE };
+	}
+
+	std::vector<Action> recursiveDLS(Node& node, Problem& problem, int limit)
+	{
+		/*
+		* Algorithm:
+			function RECURSIVE-DLS(node, problem, limit ) returns a solution, or failure/cutoff
+				if problem.GOAL-TEST(node.STATE) then return SOLUTION(node)
+				else if limit = 0 then return cutoff
+				else
+					cutoff occurred?←false
+					for each action in problem.ACTIONS(node.STATE) do
+					child ←CHILD-NODE(problem, node, action)
+					result ←RECURSIVE-DLS(child , problem, limit − 1)
+					if result = cutoff then cutoff occurred?←true
+					else if result != failure then return result
+					if cutoff occurred? then return cutoff else return failure
+
+		* Reference:
+		* Figure 3.17 A recursive implementation of depth-limited tree search.
+		* 
+		* Note:
+		* 需要使用problem.freeMemory(nodePtr)管理内存
+		*/
+		// TODO
+		return std::vector<Action>();
+	}
+
+	std::vector<Action> dls(Problem& problem, int limit)
+	{
+		Node initNode(NULL, Action::FAILURE, 0, problem.initState);
+		return recursiveDLS(initNode, problem, limit);
+	}
+
+	std::vector<Action> dlsWrapper(Problem& problem)
+	{
+		return dls(problem, 10);
+	}
+
+	std::vector<Action> aStar(Problem& problem, heuristicFunc heuristicFunc)
+	{
+		/*
+		* Algorithm:
+			function HEURISTIC-SEARCH(problem) returns a solution, or failure
+				node ←a node with STATE = problem.INITIAL-STATE, PATH-COST = 0
+				frontier ←a priority queue ordered by PATH-COST, with node as the only element
+				explored ←an empty set
+				loop do
+					if EMPTY?( frontier) then return failure
+					node←POP( frontier ) // chooses the lowest-cost node in frontier
+					if problem.GOAL - TEST(node.STATE) then return SOLUTION(node)
+					add node.STATE to explored
+					for each action in problem.ACTIONS(node.STATE) do
+						child ←CHILD - NODE(problem, node, action)
+						child.cost += heuristicFunc(child.state, problem.goalState)
+						if child.STATE is not in explored or frontier then
+							frontier ←INSERT(child, frontier)
+						else if child.STATE is in frontier with higher PATH - COST then
+							replace that frontier node with child
+
+		* Note: 
+		* The above algorithm is identical to the UNIFORM-COST-SEARCH described in Figure 3.14 except that we update the cost according to an heuristic function.
+		* You should use a priority queue to maintain the frontier.
+			std::priority_queue<Node*, std::vector<Node*>, Node::cmp> frontier;
+		* You may need a map whose key is a state and value is a pointer to node to efficiently find whether a child.STATE is not in frontier or in frontier with higher path cost.
+			std::map<std::vector<int>, Node*> stateNodePtrMap;
+		* 
+		* Reference:
+		* Figure 3.14 Uniform-cost search on a graph.
+		*/
+		// TODO
+		return { Action::FAILURE };
+	}
+	std::vector<Action> aStarMisplace(Problem& problem)
+	{
+		return aStar(problem, heuristic::misplace);
+	}
+	std::vector<Action> aStarManhattan(Problem& problem)
+	{
+		return aStar(problem, heuristic::manhattan);
+	}
+}
